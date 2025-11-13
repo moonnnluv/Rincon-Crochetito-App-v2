5eb48a4c66e41413d147ef245a2483fab4c0bd8c
moonnnluv <ale.reyesd@duocuc.cl>
2025-11-13 18:12:26 -0300

    Creación Fragments Cliente e inicio configuración main

--- app/src/main/java/com/example/rincon_crochetitov2/Cliente/MainActivityCliente.kt
+++ app/src/main/java/com/example/rincon_crochetitov2/Cliente/MainActivityCliente.kt
@@ -1,21 +1,55 @@
 package com.example.rincon_crochetitov2.Cliente
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.rincon_crochetitov2.Cliente.Fragments.FragmentInicioC
 import com.example.rincon_crochetitov2.R
+import com.example.rincon_crochetitov2.databinding.ActivityMainClienteBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivityCliente : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainClienteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main_cliente)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainClienteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+        private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navFragment, fragment)
+            .commit()
         }
+
+    }
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+
     }
 }
